c407cd6757afcde9353688a782f96fa57956f01f
svntax <endless.answers@gmail.com>
2021-04-26 20:01:56 -0400

    Set up ocean tiles.

--- Gameplay.gd
+++ Gameplay.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 onready var bg_music = $BgMusic
+onready var waves_animation_player = $WavesAnimationPlayer
 
 func _ready():
 	bg_music.play()
+	waves_animation_player.play("waves", -1, 0.5)
